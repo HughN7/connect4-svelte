608ad83ecef03338ba135446682b67cb3e931c10
Hieu Vo <hughvo39@gmail.com>
2023-06-21 19:26:34 -0700

    transitions

--- src/lib/game_components/board_slot.svelte
+++ src/lib/game_components/board_slot.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-    import { board } from "../../game_state";
+  import { board } from "../../game_state";
 
-    export let row_id: number;
-    export let column_id: number;
-
+  export let row_id: number;
+  export let column_id: number;
 </script>
 
-<div class="flex w-13 h-13 md:w-20 md:h-20 border-blue-500 border-2 bg-blue-500 justify-center items-center group-hover:bg-blue-900 group-hover:border-blue-900">
-    {#if $board[row_id][column_id] === "O"}
-    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-yellow-400"></div>
-
-    {:else if $board[row_id][column_id] === "X"}
-    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-red-600"></div>
-
-    {:else}
-    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-white"></div>
-
-    {/if}   
-    
+<div
+  class="flex ease-in duration-100 w-13 h-13 md:w-20 md:h-20 border-blue-500 border-2 bg-blue-500 justify-center items-center group-hover:bg-blue-900 group-hover:border-blue-900"
+>
+  {#if $board[row_id][column_id] === "O"}
+    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-yellow-400" />
+  {:else if $board[row_id][column_id] === "X"}
+    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-red-600" />
+  {:else}
+    <div class="w-10 h-10 md:w-16 md:h-16 rounded-full bg-white" />
+  {/if}
 </div>
